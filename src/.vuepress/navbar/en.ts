849052fa2718e35f0f999c1c6128cac5292fe605
yanglixin <yanglixin@qiniu.com>
2023-05-31 10:29:52 +0800

    update

--- src/.vuepress/navbar/en.ts
+++
@@ -1,7 +0,0 @@
-import { navbar } from "vuepress-theme-hope";
-
-export const enNavbar = navbar([
-  { text: "主页", icon: "home", link: "/" },
-  { text: "归档", icon: "discover", link: "/archives/" },
-  { text: "关于", icon: "info", link: "/intro/" },
-]);
